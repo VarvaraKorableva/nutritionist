75a0f5763ead9535a354e17d6af848509fd34530
Varvara Korableva <v.korableva1990@yandex.ru>
2025-05-09 19:07:09 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
